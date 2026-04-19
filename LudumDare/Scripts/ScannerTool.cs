b8fd93dd03fefa1c764d574daa8ffeca03947326
HollowsEdgePC <140660131+HollowsEdge@users.noreply.github.com>
2026-04-19 14:36:25 -0600

    Scanner 2

--- LudumDare/Scripts/ScannerTool.cs
+++ LudumDare/Scripts/ScannerTool.cs
@@ -6,15 +6,27 @@ public partial class ScannerTool : Node3D
     [Export] private int gridY = 50;
     [Export] private int gridX = 50;
     [Export] private int spacing = 2;
+    [Export] private float lineWidth = .1f;
+    [Export] private Mesh visualMesh;
+    [Export] private Material sphereMat;
+
 
     [Export] private bool debugRaycastPoints = false;
 
     private List<Node> previousPointMeshes = new();
-    private Vector3[,] previousPointPositions;
+    private (Vector3 position, Vector3 normal)[,] previousPointPositions;
+
+    private MultiMeshInstance3D multiMeshInstance3D = new();
+    private MultiMesh multimesh = new();
 
     public override void _Ready()
     {
-        previousPointPositions = new Vector3[gridX, gridY];
+        previousPointPositions = new (Vector3 position, Vector3 normal)[gridX, gridY];
+        GetTree().Root.CallDeferred("add_child", multiMeshInstance3D);
+        multiMeshInstance3D.Multimesh = multimesh;
+        multimesh.TransformFormat = MultiMesh.TransformFormatEnum.Transform3D;
+        multimesh.Mesh = visualMesh;
+        multimesh.InstanceCount = gridX * gridY;
     }
 
     public override void _PhysicsProcess(double delta)
@@ -27,7 +39,7 @@ public partial class ScannerTool : Node3D
                     item.QueueFree();
                 previousPointMeshes.Clear();
             }
-
+            int currIndex = 0;
             for (int x = 0; x < gridX; x++)
             {
                 for (int y = 0; y < gridY; y++)
@@ -44,25 +56,30 @@ public partial class ScannerTool : Node3D
                     var query = PhysicsRayQueryParameters3D.Create(origin, end, 0b00000000_00000000_00000000_00000001);
 
                     var result = spaceState.IntersectRay(query);
-                    previousPointPositions[x, y] = (Vector3)result["position"];
+                    previousPointPositions[x, y] = ((Vector3)result["position"], (Vector3)result["normal"]);
+                    multimesh.SetInstanceTransform(currIndex, new Transform3D(new Basis(), (Vector3)result["position"]));
+
 
                     if (debugRaycastPoints)
                     {
                         SphereMesh point = new()
                         {
-                            Radius = .01f,
-                            Height = .01f * 2
+                            Radius = .02f,
+                            Height = .02f * 2
                         };
 
                         MeshInstance3D mesh = new()
                         {
                             Mesh = point,
-                            Layers = 0b00000000_00000000_10000000_00000000
+                            Layers = 0b00000000_00000000_10000000_00000000,
+                            MaterialOverride = sphereMat
                         };
                         previousPointMeshes.Add(mesh);
                         GetTree().Root.AddChild(mesh);
                         mesh.GlobalPosition = (Vector3)result["position"];
                     }
+                    
+                    currIndex++;
                 }
 
             }
