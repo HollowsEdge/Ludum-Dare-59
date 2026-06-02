6bc0104b9bcccdb0e6551af35efd81c3b4450e82
HollowsEdgePC <140660131+HollowsEdge@users.noreply.github.com>
2026-06-02 01:13:34 -0600

    Added treasure visual on collection
    place treasure chest randomly on exit mat
    
    Previous commit fixed randomize after level generation (wrong message)

--- LudumDare/Scripts/DropTreasure.cs
+++ LudumDare/Scripts/DropTreasure.cs
@@ -2,9 +2,13 @@ using Godot;
 
 public partial class DropTreasure : Area3D
 {
+    [Export] private PackedScene treasureScene;
+    [Export] private MeshInstance3D floorMeshNode;
+
     // References
     private PlayerController player;
     private GameManager gameManager;
+    private int treasurePlaced;
 
     public override void _Ready()
     {
@@ -19,10 +23,21 @@ public partial class DropTreasure : Area3D
     /// <param name="treasure">The treasure chest to collect.</param>
     public void RecieveTreasure(Node3D treasure)
     {
+        treasurePlaced++;
         if (gameManager != null)
             gameManager.AddTreasure(treasure);
         else
             GD.PrintErr("DropTreasure: gameManager is NULL Fix this!");
+
+        // Visually place treasure on floor
+        Node3D treasureNode = treasureScene.Instantiate<Node3D>();
+        treasureNode.Name = "Treasure " + treasurePlaced;
+        floorMeshNode.AddChild(treasureNode);
+        Vector2 floorSize = ((PlaneMesh)floorMeshNode.Mesh).Size * 0.5f;
+        // Reduce size so treasure isn't off floor
+        floorSize.X -= 0.5f;
+        floorSize.Y -= 0.5f;
+        treasureNode.Position += new Vector3((float)GD.RandRange(-floorSize.X, floorSize.X), 0, (float)GD.RandRange(-floorSize.Y, floorSize.Y));
     }
 
     public override void _PhysicsProcess(double delta)
